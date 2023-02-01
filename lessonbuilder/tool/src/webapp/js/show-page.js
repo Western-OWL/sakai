431d0d5d2325f38a7dbeff3aee2b9fb9cdb8c5a6
bbailla2 <bbailla2@uwo.ca>
2023-02-01 10:27:12 -0500

    OWL-5252 - port OWL-3821 to OWL 22

--- lessonbuilder/tool/src/webapp/js/show-page.js
+++ lessonbuilder/tool/src/webapp/js/show-page.js
@@ -405,14 +405,19 @@ $(document).ready(function() {
 			resizable: false,
 			modal: true,
 			dialogClass: "no-close",
-			    buttons: [{text:msg("simplepage.delete"),
+			buttons: [{text:msg("simplepage.delete"),
+					'class':'active',
 				          click: function() {
 				          insist = true;
 				          delbutton.click();
 				      }},{text:msg("simplepage.cancel_message"),
 				          click: function() {
 				          $( this ).dialog( "close" );}}
-				]}).parent('.ui-dialog').css('zIndex',900);
+				],
+			open: function( event, ui) {
+				$(this).parent().find(".ui-dialog-buttonset").addClass("act");
+			}
+		}).parent('.ui-dialog').css('zIndex',900);
 		
 		$(window).resize(function() {
 			var modalDialogList = ['#subpage-dialog', '#edit-item-dialog', '#edit-multimedia-dialog',
