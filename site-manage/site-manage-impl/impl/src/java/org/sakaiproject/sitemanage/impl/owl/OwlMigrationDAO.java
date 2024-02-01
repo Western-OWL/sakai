a3b7b4b559acd1ed129fa8a2788ac2f08a487cbe
Brian Jones <bjones86@uwo.ca>
2024-02-01 11:36:24 -0500

    OWL-5636: OwlMigrationDAO.getEligibleTermsForMigration()

--- site-manage/site-manage-impl/impl/src/java/org/sakaiproject/sitemanage/impl/owl/OwlMigrationDAO.java
+++ site-manage/site-manage-impl/impl/src/java/org/sakaiproject/sitemanage/impl/owl/OwlMigrationDAO.java
@@ -30,6 +30,7 @@ public class OwlMigrationDAO
     private static final String OWL_MIG_SELECTIONS_WITH_VISIBLE_STATUSES    = "OWL_MIG_SELECTIONS_WITH_VISIBLE_STATUSES";
     private static final String OWL_MIG_VISIBLE_STATUSES                    = "OWL_MIG_VISIBLE_STATUSES";
     private static final String OWL_MIG_CHANGEABLE_SELECTIONS               = "OWL_MIG_CHANGEABLE_SELECTIONS";
+    private static final String OWL_MIG_ELIGIBLE_TERMS                      = "OWL_MIG_ELIGIBLE_TERMS";
 
     // Delimiters used in Admin Workspace props
     private static final String PIPE_DELIM          = "\\|"; // Pipe is a special character in regex, so it needs to be escaped
@@ -143,6 +144,16 @@ public class OwlMigrationDAO
         ResourceProperties props = site.getProperties();
         String prop = props.getProperty( sitePropKey );
         return prop == null ? Optional.empty() : Optional.of( prop );
+    }
+
+    /**
+     * Get the list of eligible term codes stored in the "OWL_MIG_ELIGIBLE_TERMS" Admin site property
+     * @return A List of Strings, where each String is a term code; all sites belonging to the term codes are eligible for migration options
+     */
+    public static List<String> getEligibleTermsForMigration()
+    {
+        // Format: <termCode1>|<termCode2>|<termCode3>
+        return parsePipeDelimitedProp( OWL_MIG_ELIGIBLE_TERMS );
     }
 
     /**
