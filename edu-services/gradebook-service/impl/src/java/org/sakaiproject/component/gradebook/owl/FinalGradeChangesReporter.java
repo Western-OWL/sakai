b8190bc9e576b35b74d52f92bbbb8c1c4ca451be
Paul Lukasewych <plukasew@uwo.ca>
2026-02-19 09:44:02 -0500

    [OWL-5728] delegate stubs

--- edu-services/gradebook-service/impl/src/java/org/sakaiproject/component/gradebook/owl/FinalGradeChangesReporter.java
+++ edu-services/gradebook-service/impl/src/java/org/sakaiproject/component/gradebook/owl/FinalGradeChangesReporter.java
@@ -1,6 +1,8 @@
 package org.sakaiproject.component.gradebook.owl;
 
+import java.util.Set;
 import org.sakaiproject.component.gradebook.GradebookServiceHibernateImpl;
+import org.sakaiproject.service.gradebook.shared.owl.finalgrades.OwlGradeSubmissionGrades;
 import org.sakaiproject.service.gradebook.shared.owl.finalgrades.report.FGChanges;
 
 /**
@@ -21,9 +23,37 @@ class FinalGradeChangesReporter
 		gbServ = gbService;
 	}
 
-	FGChanges getChanges(String siteId, String sectionId)
+	FGChanges getChanges(String siteId, String sectionEid)
 	{
-		// OWLTODO: impl based on CourseGradeSubmitter.getGradeChangeReport()
+		// overall impl based on CourseGradeSubmitter.getGradeChangeReport()
+		// 1. get the current grades
+		// 2. get the existing final grade submission
+		// 3. get the submitted grades from the existing submission
+		// 4. compare the current and previous grades and return result
+
+		return checkForChanges(getCurrentGrades(siteId, sectionEid), getPreviousGrades(siteId, sectionEid));
+	}
+
+	private Set<OwlGradeSubmissionGrades> getCurrentGrades(String siteId, String sectionEid)
+	{
+		// OWLTODO: impl...see CourseGradeSubmitter.getCurrentCourseGrades()
+		// step 1
+
+		return Set.of();
+	}
+
+	private Set<OwlGradeSubmissionGrades> getPreviousGrades(String siteId, String sectionEid)
+	{
+		// OWLTODO: impl...see CourseGradeSubmitter.getGradeChangeReport()
+		// step 2 and 3
+
+		return Set.of();
+	}
+
+	private FGChanges checkForChanges(Set<OwlGradeSubmissionGrades> currentGrades, Set<OwlGradeSubmissionGrades> previousGrades)
+	{
+		// OWLTODO: impl...see CourseGradeSubmitter.checkForGradeChanges()
+		// step 4
 
 		return new FGChanges(0, 0, 0);
 	}
